4561d50f0173beb71e50041bb8f82b0434e58b23
marcusdesantis <117993722+HoracioLobatoAbisoft@users.noreply.github.com>
2023-04-21 15:56:39 -0500

    added validations register form

--- src/components/Login/components/RegisterForm.tsx
+++ src/components/Login/components/RegisterForm.tsx
@@ -8,7 +8,8 @@ import { Link } from "react-router-dom";
 
 const RegisterForm = () => {
   const [client, setClient] = useState("");
-
+  const expRegValidacion = /^(?!.*(.)(\1)).*(?<![^aeiouAEIOUyY\s]{3})[a-zA-Z]*([aeiouAEIOUyY][a-zA-Z]*([aeiouAEIOUyY][a-zA-Z]*)?)?(\s[a-zA-Z]*([aeiouAEIOUyY][a-zA-Z]*([aeiouAEIOUyY][a-zA-Z]*)?)?)*$/;
+  const expRegValidacionSinNumeros = /^(?!.*(.)(\1))[a-zA-Z\s]*(?<![^aeiouAEIOUyY\s]{3})[a-zA-Z]*([aeiouAEIOUyY][a-zA-Z]*([aeiouAEIOUyY][a-zA-Z]*)?)?(\s[a-zA-Z]*([aeiouAEIOUyY][a-zA-Z]*([aeiouAEIOUyY][a-zA-Z]*)?)?)*$/;
   const handleChange = (event: any) => {
     setClient(event.target.value);
   };
@@ -17,10 +18,11 @@ const RegisterForm = () => {
     register,
     handleSubmit,
     watch,
-    formState: { errors },
+    formState: { errors }
   } = useForm();
 
   const onSubmit = (data: any) => {
+    console.log("data");
     console.log(data);
   };
 
@@ -87,8 +89,9 @@ const RegisterForm = () => {
                 type="text"
                 className="text-sm placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 w-full py-2 focus:outline-none focus:border-[#f58220]"
                 placeholder="Inserisci la Ragione Sociale dell'azienda"
-                {...register("Ragione Sociale", { required: true })}
+                {...register("RagioneSociale", { required: true, pattern: expRegValidacion })}
               />
+              {errors.RagioneSociale && <p className="text-red-500 text-sm">Ragione Sociale no valido</p>}
             </div>
           </div>
 
@@ -100,8 +103,8 @@ const RegisterForm = () => {
               <select
                 id="countries"
                 className="bg-gray-50 focus:outline-none border border-gray-300 text-sm rounded-2xl focus:border-[#f58220]  block w-full p-2.5"
+                {...register("tipoAttivita")}
               >
-                <option selected>Agenzia di Cominicazione</option>
                 <option value="1">Agenzia di Comunicazione</option>
                 <option value="2">
                   Altro professionista delle Arti Grafiche
@@ -135,8 +138,9 @@ const RegisterForm = () => {
                   className="text-sm placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 w-full py-2 focus:outline-none focus:border-[#f58220]"
                   id="exampleInputPassword1"
                   placeholder="Inserisci il tuo Nome"
-                  {...register("Nome", { required: true })}
+                  {...register("Nome", { required: true, pattern: expRegValidacionSinNumeros })}
                 />
+                {errors.Nome && <p className="text-red-500 text-sm">Nome no valido</p>}
               </div>
             </div>
             <div className="w-full sm:w-1/2">
@@ -151,8 +155,9 @@ const RegisterForm = () => {
                   className="text-sm placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 w-full py-2 focus:outline-none focus:border-[#f58220]"
                   id="exampleInputPassword1"
                   placeholder="Inserisci il tuo Cognome"
-                  {...register("Cognome", { required: true })}
+                  {...register("Cognome", { required: true, pattern: expRegValidacionSinNumeros })}
                 />
+                {errors.Cognome && <p className="text-red-500 text-sm">Cognome no valido</p>}
               </div>
             </div>
           </div>
@@ -166,8 +171,9 @@ const RegisterForm = () => {
                 type="text"
                 className="text-sm placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 w-full py-2 focus:outline-none focus:border-[#f58220]"
                 placeholder="Inserisci I'indirizzo"
-                {...register("Indirizzo", { required: true })}
+                {...register("Indirizzo", { required: true, pattern: expRegValidacion })}
               />
+              {errors.Indirizzo && <p className="text-red-500 text-sm">Indirizzo no valido</p>}
             </div>
           </div>
           <p className="text-sm mt-2">
@@ -181,6 +187,7 @@ const RegisterForm = () => {
             <select
               id="countries"
               className="bg-gray-50 focus:outline-none border border-gray-300 text-sm rounded-2xl focus:border-[#f58220]  block w-full p-2.5"
+              {...register("countries", { required: true })}
             >
               <option value="US">Italia</option>
               <option value="CA">Albania</option>
@@ -196,6 +203,7 @@ const RegisterForm = () => {
               <option value="DE">Svezia</option>
               <option value="DE">Svizzera</option>
             </select>
+            {errors.countries && <p className="text-red-500 text-sm">countries no valido</p>}
           </div>
 
           <div className="mt-4 flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
@@ -210,8 +218,9 @@ const RegisterForm = () => {
                   className="text-sm placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 w-full py-2 focus:outline-none focus:border-[#f58220]"
                   id="exampleInputPassword1"
                   placeholder="Inserisci il CAP"
-                  {...register("Cap", { required: true })}
+                  {...register("Cap", { required: true, pattern: expRegValidacion })}
                 />
+                {errors.Cap && <p className="text-red-500 text-sm">Cap no valido</p>}
               </div>
             </div>
             <div className="w-full sm:w-1/2">
@@ -226,8 +235,9 @@ const RegisterForm = () => {
                   className="text-sm placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 w-full py-2 focus:outline-none focus:border-[#f58220]"
                   id="exampleInputPassword1"
                   placeholder="Inserisci il tuo Cognome"
-                  {...register("Localita", { required: true })}
+                  {...register("Localita", { required: true, pattern: expRegValidacion })}
                 />
+                {errors.Localita && <p className="text-red-500 text-sm">Localita no valido</p>}
               </div>
             </div>
           </div>
@@ -247,8 +257,9 @@ const RegisterForm = () => {
                   className="text-sm w-full placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 py-2 focus:outline-none focus:border-[#f58220]"
                   id="exampleInputPassword1"
                   placeholder="Inserisci il CAP"
-                  {...register("pIva", { required: true })}
+                  {...register("pIva", { pattern: expRegValidacion })}
                 />
+                {errors.pIva && <p className="text-red-500 text-sm">P. Iva no valido</p>}
               </div>
             </div>
             <div className="w-full sm:w-1/2">
@@ -262,8 +273,9 @@ const RegisterForm = () => {
                   className="text-sm placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 w-full py-2 focus:outline-none focus:border-[#f58220]"
                   id="exampleInputPassword1"
                   placeholder="Inserisci il Codice Fiscale"
-                  {...register("codiceFiscale", { required: true })}
+                  {...register("codiceFiscale", { required: true, pattern: expRegValidacion })}
                 />
+                {errors.codiceFiscale && <p className="text-red-500 text-sm">Codice Fiscale no valido</p>}
               </div>
             </div>
           </div>
@@ -274,14 +286,16 @@ const RegisterForm = () => {
                 <div className="inline-flex items-center justify-center absolute mt-2.5 left-0 top-0 h-ful w-10 text-gray-400">
                   <i className="fas fa-lock text-[#f58220]"></i>
                 </div>
-
-                <input
-                  type="text"
-                  className="text-sm w-full placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 py-2 focus:outline-none focus:border-[#f58220]"
-                  id="exampleInputPassword1"
-                  placeholder="Inserisci la casella PEC"
-                  {...register("pec", { required: true })}
-                />
+                <div className="w-full">
+                  <input
+                    type="text"
+                    className="text-sm w-full placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 py-2 focus:outline-none focus:border-[#f58220]"
+                    id="exampleInputPassword1"
+                    placeholder="Inserisci la casella PEC"
+                    {...register("pec", { required: true, pattern: expRegValidacion })}
+                  />
+                  {errors.pec && <p className="text-red-500 text-sm">Pec Fiscale no valido</p>}
+                </div>
               </div>
             </div>
             <div className="w-full sm:w-1/2">
@@ -295,8 +309,9 @@ const RegisterForm = () => {
                   className="text-sm placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 w-full py-2 focus:outline-none focus:border-[#f58220]"
                   id="exampleInputPassword1"
                   placeholder="Inserisci il Codice SDI"
-                  {...register("codiceSdi", { required: true })}
+                  {...register("codiceSdi", { pattern: expRegValidacion })}
                 />
+                {errors.codiceSdi && <p className="text-red-500 text-sm">Codice SDI Fiscale no valido</p>}
               </div>
             </div>
           </div>
@@ -310,19 +325,21 @@ const RegisterForm = () => {
                 <div className="inline-flex items-center justify-center absolute mt-2.5 left-0 top-0 h-ful w-10 text-gray-400">
                   <i className="fas fa-lock text-[#f58220]"></i>
                 </div>
-
-                <input
-                  type="text"
-                  className="text-sm w-full placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 py-2 focus:outline-none focus:border-[#f58220]"
-                  id="exampleInputPassword1"
-                  placeholder="Inserisci un numero di rete fissa"
-                  {...register("telefono", { required: true })}
-                />
+                <div className="w-full">
+                  <input
+                    type="tel"
+                    className="text-sm w-full placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 py-2 focus:outline-none focus:border-[#f58220]"
+                    id="exampleInputPassword1"
+                    placeholder="Inserisci un numero di rete fissa"
+                    {...register("telefono", { required: true })}
+                  />
+                  {errors.telefono && <p className="text-red-500 text-sm">Telefono no valido</p>}
+                </div>
               </div>
             </div>
             <div className="w-full sm:w-1/2">
               <label>Cellulare</label>
-              <div className="relative">
+              <div className="tel">
                 <div className="inline-flex items-center justify-center absolute mt-2.5 left-0 top-0 h-ful w-10 text-gray-400">
                   <i className="fas fa-lock text-[#f58220]"></i>
                 </div>
@@ -331,8 +348,9 @@ const RegisterForm = () => {
                   className="text-sm placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 w-full py-2 focus:outline-none focus:border-[#f58220]"
                   id="exampleInputPassword1"
                   placeholder="Inserisci un numero Cellulare"
-                  {...register("cellulare", { required: true })}
+                  {...register("cellulare")}
                 />
+                {errors.cellulare && <p className="text-red-500 text-sm">Cellulare no valido</p>}
               </div>
             </div>
           </div>
@@ -349,8 +367,9 @@ const RegisterForm = () => {
                   className="text-sm w-full placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 py-2 focus:outline-none focus:border-[#f58220]"
                   id="exampleInputPassword1"
                   placeholder="Inserisci un numero di Fax"
-                  {...register("fax", { required: true })}
+                  {...register("fax")}
                 />
+                {errors.fax && <p className="text-red-500 text-sm">Fax no valido</p>}
               </div>
             </div>
             <div className="w-full sm:w-1/2">
@@ -360,12 +379,13 @@ const RegisterForm = () => {
                   <i className="fas fa-lock text-[#f58220]"></i>
                 </div>
                 <input
-                  type="text"
+                  type="url"
                   className="text-sm placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 w-full py-2 focus:outline-none focus:border-[#f58220]"
                   id="exampleInputPassword1"
                   placeholder="Inserisci il tuo sito internet"
-                  {...register("cellulare", { required: true })}
+                  {...register("sitoInternet")}
                 />
+                {errors.sitoInternet && <p className="text-red-500 text-sm">Sito Internet no valido</p>}
               </div>
             </div>
           </div>
@@ -384,13 +404,16 @@ const RegisterForm = () => {
                   <i className="fas fa-lock text-[#f58220]"></i>
                 </div>
 
-                <input
-                  type="text"
-                  className="text-sm w-full placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 py-2 focus:outline-none focus:border-[#f58220]"
-                  id="exampleInputPassword1"
-                  placeholder="Inserisci la tua Email"
-                  {...register("email", { required: true })}
-                />
+                <div className="w-full">
+                  <input
+                    type="email"
+                    className="text-sm w-full placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 py-2 focus:outline-none focus:border-[#f58220]"
+                    id="exampleInputPassword1"
+                    placeholder="Inserisci la tua Email"
+                    {...register("email", { required: true })}
+                  />
+                  {errors.email && <p className="text-red-500 text-sm">Email no valido</p>}
+                </div>
               </div>
             </div>
             <div className="w-full sm:w-1/2">
@@ -400,12 +423,13 @@ const RegisterForm = () => {
                   <i className="fas fa-lock text-[#f58220]"></i>
                 </div>
                 <input
-                  type="text"
+                  type="email"
                   className="text-sm placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 w-full py-2 focus:outline-none focus:border-[#f58220]"
                   id="exampleInputPassword1"
                   placeholder="Ripeti la tua Email"
                   {...register("rEmail", { required: true })}
                 />
+                {errors.rEmail && <p className="text-red-500 text-sm">Email no valido</p>}
               </div>
               <p className="flex justify-end mt-1">
                 (*) indica un campo obbligatorio
@@ -419,21 +443,21 @@ const RegisterForm = () => {
               className="flex px-8 mt-4 items-center justify-center focus:outline-none text-white text-sm sm:text-base bg-[#f58220] hover:bg-blue-6 rounded-2xl py-2 transition duration-150 ease-in"
             >
               Registrati
-            </button>           
+            </button>
           </div>
           <div className=" flex justify-center mt-10 items-center space-x-2">
-              <p>
-                <span className="font-semibold">Sei già registrato?</span>
-              </p>
-              <div className="flex justify-center">
-                <Link
-                  to="/Login"
-                  className="flex font-semibold items-center justify-center focus:outline-none hover:underline text-lg sm:text-base text-[#f58220] rounded-2xl py-2 transition duration-150 ease-in"
-                >
-                  Accedi
-                </Link>
-              </div>
+            <p>
+              <span className="font-semibold">Sei già registrato?</span>
+            </p>
+            <div className="flex justify-center">
+              <Link
+                to="/Login"
+                className="flex font-semibold items-center justify-center focus:outline-none hover:underline text-lg sm:text-base text-[#f58220] rounded-2xl py-2 transition duration-150 ease-in"
+              >
+                Accedi
+              </Link>
             </div>
+          </div>
           <div className="mt-4">
             <p>Cliccando sul pulsante "Registrati", acconsento che:</p>
             <div className="flex items-center mb-4 mt-2">
