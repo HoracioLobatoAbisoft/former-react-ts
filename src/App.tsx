820f0f4f9082c991a0c29d9a875f3d90200e4584
marcusdesantis <117993722+HoracioLobatoAbisoft@users.noreply.github.com>
2023-05-03 07:29:17 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,11 @@ import GlosarioTipografico from "./components/GlosarioTipografico/GlosarioTipogr
 function App() {
   // const location = useLocation();
 
-  // const searchParams = new URLSearchParams(location.search);
-  //   const token = searchParams.get('token') ?? '';
+   const searchParams = new URLSearchParams(location.search);
+    const token = searchParams.get('token') ?? '';
+    const idUtd = searchParams.get('id') ?? '';
+    console.log("Token: "+token);
+    console.log("IdUt: "+idUtd);
   //   //dataStore.setToken();
   //   localStorage.setItem("token",token);
 
